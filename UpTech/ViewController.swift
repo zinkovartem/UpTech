ef3ee227af6fa07fa1aba5d7569724fbe81970b3
Artem Zinkov <Ar.Zinkov@kasta.ua>
2019-07-12 17:18:41 +0300

    Initial Commit

---
+++ UpTech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpTech
+//
+//  Created by A.Zinkov on 7/12/19.
+//  Copyright © 2019 ArtemZinkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
